1545535cfa5867fcb39986a1c0521ca47ece4488
Samuelimmerso <samuelimmerso.info@gmail.com>
2023-01-27 17:05:43 +0100

    indentation

--- jsSnack3/main.js
+++ jsSnack3/main.js
@@ -1,19 +1,19 @@
 //declarations
 let array = [];
-let arraylen = Math.floor(Math.random() * 10)+2;
-let sum=0;
+let arraylen = Math.floor(Math.random() * 10) + 2;
+let sum = 0;
 
 //random assignaments
-for(let i=0; i < arraylen; i++){
+for (let i = 0; i < arraylen; i++) {
     array[i] = Math.floor(Math.random() * 100);
 }
 
 //sum calculation and number prints
-for(let i=0; i < array.length; i++){
-    if((i+1) % 2 != 0){
-        sum+=array[i];
+for (let i = 0; i < array.length; i++) {
+    if ((i + 1) % 2 != 0) {
+        sum += array[i];
     }
-    console.log(i+1 + " Numero: " + array[i]);
+    console.log(i + 1 + " Numero: " + array[i]);
 }
 
 //sum print
