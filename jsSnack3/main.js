a77381158ee916a8b5e84f2aef0bd9e1a8ae2ea8
Samuelimmerso <samuelimmerso.info@gmail.com>
2023-01-27 16:38:48 +0100

    end

--- jsSnack3/main.js
+++ jsSnack3/main.js
@@ -1,18 +1,21 @@
+//declarations
 let array = [];
 let arraylen = Math.floor(Math.random() * 10)+2;
 let sum=0;
 
-
+//random assignaments
 for(let i=0; i < arraylen; i++){
     array[i] = Math.floor(Math.random() * 100);
 }
 
+//sum calculation and number prints
 for(let i=0; i < array.length; i++){
     sum+=array[i];
-    console.log()
+    console.log(i+1 + " Numero: " + array[i]);
 }
 
-console.log(sum);
+//sum print
+console.log("Somma: " + sum);
 
 
 
